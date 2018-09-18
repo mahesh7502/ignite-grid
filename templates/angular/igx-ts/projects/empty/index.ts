b83a904c299f7ec203efba0582dcdc410492ccac
Damyan Petev <damyan.petev@gmail.com>
2018-09-18 19:40:22 +0300

    feat(projects): [WIP] split igx project into base and sid-nav extension

--- templates/angular/igx-ts/projects/empty/index.ts
+++ templates/angular/igx-ts/projects/empty/index.ts
@@ -1,7 +1,8 @@
 import * as path from "path";
 import { Util } from "../../../../../lib/Util";
+import { BaseIgxProject } from "../base";
 
-class EmptyJsBlocksProject implements ProjectTemplate {
+class EmptyJsBlocksProject extends BaseIgxProject implements ProjectTemplate {
 	public id: string = "angular";
 	public name = "empty";
 	public description = "Default empty angular project structure for Ignite UI for Angular";
@@ -10,32 +11,7 @@ class EmptyJsBlocksProject implements ProjectTemplate {
 	public projectType: string = "igx-ts";
 	public hasExtraConfiguration: boolean = false;
 
-	private CUSTOM_THEME
-		= `/* See: https://www.infragistics.com/products/ignite-ui-angular/angular/components/themes.html */
-@import "~igniteui-angular/lib/core/styles/themes/index";
-
-$primary: #731963 !default;
-$secondary: #ce5712 !default;
-
-$app-palette: igx-palette($primary, $secondary);
-
-@include igx-core();
-@include igx-theme($app-palette);
-`;
-	private DEFAULT_THEME = `,
-              "node_modules/igniteui-angular/styles/igniteui-angular.css"`;
-
-	public installModules(): void {
-		throw new Error("Method not implemented.");
-	}
-	public upgradeIgniteUIPackage(projectPath: string, packagePath: string): void {
-		// noop
-	}
-	public getExtraConfiguration(): ControlExtraConfiguration[] {
-		return [];
-	}
-	public setExtraConfiguration(extraConfigKeys: any[]) { }
-	public generateFiles(outputPath: string, name: string, theme: string, ...options: any[]): Promise<boolean> {
+	public async generateFiles(outputPath: string, name: string, theme: string, ...options: any[]): Promise<boolean> {
 		const config = {
 			"$(CustomTheme)": "",
 			"$(DefaultTheme)": "",
@@ -45,19 +21,13 @@ $app-palette: igx-palette($primary, $secondary);
 			"$(theme)": theme,
 			"__path__": name
 		};
-
-		switch (theme) {
-			case "Custom":
-				config["$(CustomTheme)"] = this.CUSTOM_THEME;
-				break;
-			case "Default":
-			default:
-				config["$(DefaultTheme)"] = this.DEFAULT_THEME;
-				break;
-		}
-
 		const pathsConfig = {};
+
+		if (!await super.generateFiles(outputPath, name, theme, ...options)) {
+			// TODO ?
+			return false;
+		}
 		return Util.processTemplates(path.join(__dirname, "./files"), path.join(outputPath, name), config, pathsConfig);
 	}
 }
-module.exports = new EmptyJsBlocksProject();
+export default new EmptyJsBlocksProject();
